63498743004eed68faa76b04494e5d1a50882c27
Leksina Anna <annushkahorror@gmail.com>
2022-04-14 23:42:43 +0500

    правки в схеме токена

--- src/server/modules/domains/tokens/orm-entities/token-orm.entity.ts
+++ src/server/modules/domains/tokens/orm-entities/token-orm.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 import { RefreshToken, Token } from '../entities/token.entity';
 import { UserId } from '../../users/entities/user.entity';
 
-@Entity({ name: 'tokens', schema: 'public' })
+@Entity({ name: 'tokens', schema: 'core' })
 export class TokenOrmEntity implements Token {
     @PrimaryColumn({ name: 'token_id', type: 'uuid' })
     tokenId: string;
